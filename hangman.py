6e0acafc3a52eb6ca8e43ccded25630c92a69072
Dawin Ye <59813280+yeindaw@users.noreply.github.com>
2021-02-11 23:35:24 -0500

    Add files via upload
    Not completed yet

---
+++ hangman.py
@@ -0,0 +1,16 @@
+import random
+from words import word_list
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("Let's play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
